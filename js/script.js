7a32e5e8c6460e61dc44fb2af49b610d66396fe7
mahamudul-alam <alam.mmahamudul@gmail.com>
2025-08-28 17:47:02 +0300

    Some call button features implemented

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+/* Love icon count feature  */
 let totalClick = 0;
 
 let iconHits = document.querySelectorAll(".love-button");
@@ -9,3 +11,38 @@ for(const iconHit of iconHits){
 
     })
 }
+
+
+/* Call button feature  */
+
+let callButtons = document.querySelectorAll(".call-button");
+
+
+  for (const callButton of callButtons) {
+    callButton.addEventListener("click", function () {
+      
+      let cardBody = callButton.parentElement.parentElement;
+
+      
+      let service = cardBody.querySelector(".service-name").innerText;
+      let number = cardBody.querySelector(".service-number").innerText;
+
+      
+      alert('Calling ' + service + "\n" + number);
+
+        let coins= parseInt(document.getElementById("available-coin").innerText);
+
+        if(coins >= 20){
+            let currentCoins = coins - 20;
+            let currentBalance = document.getElementById("available-coin").innerText = currentCoins;
+            currentBalance.innerText = currentCoins;
+        }else{
+            alert("You account balance is running low. Please recharge coins")
+            return;
+        }
+        
+       
+
+        
+    });
+  }
